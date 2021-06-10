50e896e75a5c148c8883f004afc759e16e455d91
Danilus-s <krukovdaniil98@gmail.com>
2021-06-10 22:30:07 +0300

    Upd

--- bin/apt.lua
+++ bin/apt.lua
@@ -122,7 +122,7 @@ elseif args[1] == "install" and args[2] ~= nil then
         end
     end
     if get == nil then print("Package `" .. args[2] .. "' not found");return end
-    local url = "https://raw.githubusercontent.com/Danilus-s/OC-APT/master/" .. get[2]
+    local url = "https://raw.githubusercontent.com/" .. get[2]
     local files = {}
     io.write("Get information from: " .. url .. "\n")
     os.sleep(0)
